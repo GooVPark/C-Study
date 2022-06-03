176ae84cf83831a746f2b6781d01b396800faa12
GoovPark <znlsznzld@naver.com>
2022-06-03 12:19:27 +0900

    2022/06/03 수업예제

--- Ch03/1_If.cs
+++ Ch03/1_If.cs
@@ -4,9 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * 날짜: 2022/06/02
+ * 이름: 박성진
+ * 내용: 조건문 실습 교재 p127
+ */
+
 namespace Ch03
 {
     internal class _1_If
     {
+        static void Main1(string[] args)
+        {
+            //if
+            int num1 = 1;
+            int num2 = 2;
+
+            if(num1 < num2)
+            {
+                Console.WriteLine("num1은 num2보다 작다");
+            }
+            if (num1 > num2)
+            {
+                Console.WriteLine("num1은 num2보다 크다");
+            }
+
+            if(num1 > 0)
+            {
+                if(num2 > 1)
+                {
+                    Console.WriteLine("num1은 0보다 크고, num2는 1보다 크다");
+                }
+            }
+
+            if(num1 > 0 && num2 > 1)
+            {
+                Console.WriteLine("num1은 0보다 크고, num2는 1보다 크다");
+            }
+
+
+            //if - else
+            if(num1 > num2)
+            {
+                Console.WriteLine("num1은 num2보다 크다");
+            }
+            else
+            {
+                Console.WriteLine("num1은 num2보다 작다");
+            }
+
+            //if - else if - else
+            int num3 = 10;
+            int num4 = 20;
+            int num5 = 30;
+            int num6 = 40;
+
+            if(num3 > num4)
+            {
+                Console.WriteLine("num3가 num4보다 크다");
+            }
+            else if(num4 > num5)
+            {
+                Console.WriteLine("num4가 num5보다 크다");
+            }
+            else if(num5 > num6)
+            {
+                Console.WriteLine("num5가 num6보다 크다");
+            }
+            else
+            {
+                Console.WriteLine("num6가 가장 크다");
+            }
+        }
     }
 }
--- Ch03/2_Switch.cs
+++ Ch03/2_Switch.cs
@@ -4,9 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * 날짜: 2022/06/03
+ * 이름: 박성진
+ * 내용: 조건문 switch 실습 교재 p127
+ */
+
 namespace Ch03
 {
     internal class _2_Switch
     {
+        static void Main(string[] args)
+        {
+            Console.Write("점수 입력: ");
+            string strScore = Console.ReadLine();
+
+            int score = int.Parse(strScore);
+            int grade = score / 10;
+
+            char rank;
+            switch(grade)
+            {
+                case 10:
+                case 9:
+                    rank = 'A';
+                    break;
+                case 8:
+                    rank = 'B';
+                    break;
+                case 7:
+                    rank = 'C';
+                    break;
+                case 6:
+                    rank = 'D';
+                    break;
+                default:
+                    rank = 'F';
+                    break;
+            }
+
+            Console.WriteLine($"{rank} 입니다.");
+        }
     }
 }
