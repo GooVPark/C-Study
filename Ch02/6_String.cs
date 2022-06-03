685ddddb5d9d98711f04561444b3420fbe225f1f
GoovPark <znlsznzld@naver.com>
2022-06-03 11:09:08 +0900

    2022/06/03 수업예제

--- Ch02/6_String.cs
+++ Ch02/6_String.cs
@@ -30,9 +30,59 @@ namespace Ch02
             Console.WriteLine();
 
             //IndexOf
+            Console.WriteLine($"IndexOf: 'G': {greeting.IndexOf('G')}");
+            Console.WriteLine($"IndexOf: \"G\": {greeting.IndexOf("G")}");
+            Console.WriteLine($"IndexOf: 'o': {greeting.IndexOf('o')}");
+            Console.WriteLine($"IndexOf: \"Mor\": {greeting.IndexOf("Mor")}");
+            Console.WriteLine($"IndexOf: \"ing\": {greeting.IndexOf("ing")}");
+            Console.WriteLine();
+
             //LastIndexOf
+            Console.WriteLine($"LastIndexOf 'G': {greeting.LastIndexOf('G')}");
+            Console.WriteLine($"LastIndexOf: \"G\": {greeting.LastIndexOf("G")}");
+            Console.WriteLine($"LastIndexOf: 'o': {greeting.LastIndexOf('o')}");
+            Console.WriteLine($"LastIndexOf: \"Mor\": {greeting.LastIndexOf("Mor")}");
+            Console.WriteLine($"LastIndexOf: \"ing\": {greeting.LastIndexOf("ing")}");
+            Console.WriteLine();
+
             //Substring
+            Console.WriteLine($"Substring(0, 4): {greeting.Substring(0, 4)}");
+            Console.WriteLine($"Substring(5, 7): {greeting.Substring(5, 7)}");
+            Console.WriteLine($"Substring(5): {greeting.Substring(5)}");
+            Console.WriteLine();
+
             //Replace
+            string replaced = greeting.Replace("Morning", "Afternoon");
+            Console.WriteLine($"Replaced: {replaced}");
+            Console.WriteLine();
+
+            //기본형 데이터를 문자열로 변환
+            int var1 = 1;
+            double var2 = 2.13;
+            bool var3 = true;
+
+            string str1 = var1.ToString();
+            string str2 = var2.ToString();
+            string str3 = var3.ToString();
+
+            Console.WriteLine($"str1: {str1}");
+            Console.WriteLine($"str2: {str2}");
+            Console.WriteLine($"str3: {str3}");
+            Console.WriteLine();
+
+            //문자열 데이터를 기본형으로 변환
+            string str4 = "3";
+            string str5 = "3.14";
+            string str6 = "false";
+
+            int var4 = int.Parse(str4);
+            double var5 = double.Parse(str5);
+            bool var6 = bool.Parse(str6);
+
+            Console.WriteLine($"var4: {var4}");
+            Console.WriteLine($"var5: {var5}");
+            Console.WriteLine($"var6: {var6}");
+            Console.WriteLine();
         }
     }
 }
